9a32aa684dd01db846ca99d7cc520f631321d344
mmahali <mmahali@dxc.com>
2022-04-21 19:25:00 +0530

    update with post, delete

--- src/main/java/com/dxc/lma/controller/GreetingController.java
+++ src/main/java/com/dxc/lma/controller/GreetingController.java
@@ -34,13 +34,4 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
-    
-    
-    @Operation(summary = "Returns a greeting message")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = Greeting.class))) })
-    @GetMapping(value = "/message01", produces = { "application/json" })
-    public Greeting greeting01(@RequestParam(value = "name", defaultValue = "Manoj") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
 }
